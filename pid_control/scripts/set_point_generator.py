e21f02dbc9ab80f1eb20a3cb53815622f769df6c
Emilio Soriano <90633698+EmSor@users.noreply.github.com>
2023-03-07 16:58:16 -0600

    Update set_point_generator.py

--- pid_control/scripts/set_point_generator.py
+++ pid_control/scripts/set_point_generator.py
@@ -4,7 +4,8 @@ import numpy as np
 from pid_control.msg import set_point
 
 # Setup Variables, parameters and messages to be used (if required)
-
+hz = rospy.get_param("/rate", 0)
+amp = rospy.get_param("/amp", 0)
 
 #Stop Condition
 def stop():
@@ -15,17 +16,20 @@ def stop():
 if __name__=='__main__':
     #Initialise and Setup node
     rospy.init_node("Set_Point_Generator")
-    rate = rospy.Rate(100)
+    rate = rospy.Rate(hz)
     rospy.on_shutdown(stop)
 
     #Setup Publishers and subscribers here
-
-
+    sp = rospy.Publisher("set_point", set_point, queue_size = 10)
+    
     print("The Set Point Genertor is Running")
 
     #Run the node
     while not rospy.is_shutdown():
 
         #Write your code here
-
+        msg = set_point()
+        msg.time = rospy.get_time()
+        msg.set_point = amp*np.sin(rospy.get_time())
+        sp.publish(msg)
         rate.sleep()
